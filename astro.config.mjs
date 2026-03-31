f025ddb2fce613cf657b23cacd091d7dcafcc310
Dennis Henle <henledennis@gmail.com>
2026-03-31 16:41:25 +0200

    feat: added .github deploy config for github pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,7 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    site: 'https://dennishenle.github.io',
+    base: '/wir-heiraten',
+});
