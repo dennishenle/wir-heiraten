9bcbeb13d2e13a7a39489617876428b07ef9a899
dennishenle <henledennis@gmail.com>
2026-04-02 21:17:50 +0200

    feat: added timeline component

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,4 @@
 export { default as Header } from "./header/Header.astro";
 export { default as Hero } from "./hero/Hero.astro";
 export { default as LoveStory } from "./love-story/LoveStory.astro";
+export { default as Timeline } from "./timeline/Timeline.astro";
