121102deda03ce7da4a8fcc26759e6d9f952dfe8
dennishenle <henledennis@gmail.com>
2026-04-02 15:36:50 +0200

    feat: added svenjas story

--- src/components/love-story/stories.ts
+++ src/components/love-story/stories.ts
@@ -6,9 +6,9 @@ export const stories = [
         alt: "Wie wir uns kennenlernten",
         title: "Wie alles begann",
         text: `Es war ein ganz normaler Sommertag, als sich unsere Wege zum ersten Mal kreuzten.
-			Ich hatte Lust auf eine Quarktasche und spazierte in den Netto um dort die Theke der Bäckerei zu begutachten.
+			Ich hatte Lust auf eine Quarktasche und spazierte in den Netto, um dort die Theke der Bäckerei zu begutachten.
 			Zu meiner Überraschung fand ich leider keine Quarktasche, aber eine überaus süße Verkäuferin mit einem wundervollen Lächeln.
-			Ich bemerkte direkt, dass sie etwas besonderes an sich hatte und konnte nicht anders, als nach ihrem Facebook Profil zu fragen
+			Ich bemerkte direkt, dass sie etwas besonderes an sich hat und konnte nicht anders, als nach ihrem Facebook Profil zu fragen,
 			um sie ein paar Tage später auf ein Date einzuladen.
 			Und so begann unsere Geschichte.`,
         author: "Dennis",
@@ -17,11 +17,13 @@ export const stories = [
         image: engagementPhoto,
         alt: "Der Heiratsantrag",
         title: "Der Antrag",
-        text: `Nach Jahren voller gemeinsamer Abenteuer und unvergesslicher Momente
-			war es an der Zeit, den nächsten Schritt zu wagen. An einem
-			wunderschönen Abend unter dem Sternenhimmel kam dann die große
-			Frage — und natürlich war die Antwort ein überglückliches Ja!
-			Seitdem planen wir voller Freude den schönsten Tag unseres Lebens.`,
+        text: `Nachdem das Wetter endlich gut genug war, um wieder auf das Rubihorn zu wandern, machten wir uns sofort auf den Weg. 
+            Der Weg war wie immer sehr schön und wir machten mit einer süßen Kröte Bekanntschaft. Als wir nach dem steilen Ansteig 
+            endlich am Gipfelkreuz angelangt waen, wunderte ich mich sehr, dass Dennis wollte, 
+            dass jemand ein Foto von uns macht (normalerweise mag er das gar nicht). Zu allem Überfluss wollte er dann auch noch, dass wir unsere Rucksäcke abnehmen, 
+            obwohl ich einfach nur fertig war, mich setzen und etwas essen wollte. Er nahm trotzdem den Rucksack ab und holte ein kleines weißes Schächtelchen heraus. 
+            Da wusste ich sofort, was er vorhat. Noch bevor er auf die Knie gehen konnte, hatte ich bereits Tränen in den Augen.
+            Der Antrag war perfekt.`,
         author: "Svenja",
     },
 ];
