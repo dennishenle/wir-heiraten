125cdc451695157041f82c0f4fee351975fe3ce6
dennishenle <henledennis@gmail.com>
2026-04-03 09:27:56 +0200

    feat: added wedding detail section

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,3 +2,4 @@ export { default as Header } from "./header/Header.astro";
 export { default as Hero } from "./hero/Hero.astro";
 export { default as LoveStory } from "./love-story/LoveStory.astro";
 export { default as Timeline } from "./timeline/Timeline.astro";
+export { default as WeddingDetails } from "./details/WeddingDetails.astro";
