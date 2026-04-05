f914045d23a8b6c8da8b5cf8ef915a4d8774c7db
dennishenle <henledennis@gmail.com>
2026-04-05 18:47:58 +0200

    feat: adjusted entries to justify content in the center

--- src/components/registry/registries.ts
+++ src/components/registry/registries.ts
@@ -10,14 +10,4 @@ export const registries: Registry[] = [
 		imageUrl: "https://picsum.photos/seed/registry-amazon/400/200",
 		href: "#",
 	},
-	{
-		name: "Westwing",
-		imageUrl: "https://picsum.photos/seed/registry-westwing/400/200",
-		href: "#",
-	},
-	{
-		name: "IKEA",
-		imageUrl: "https://picsum.photos/seed/registry-ikea/400/200",
-		href: "#",
-	},
 ];
