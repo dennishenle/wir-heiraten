a5b2a9bd1e87456bb378c113bcbfa73d1b32a709
dennishenle <henledennis@gmail.com>
2026-04-05 18:53:00 +0200

    feat: added link label parameter to registry card

--- src/components/registry/registries.ts
+++ src/components/registry/registries.ts
@@ -2,6 +2,7 @@ export interface Registry {
 	name: string;
 	imageUrl: string;
 	href: string;
+	linkLabel: string;
 }
 
 export const registries: Registry[] = [
@@ -9,5 +10,12 @@ export const registries: Registry[] = [
 		name: "Amazon",
 		imageUrl: "https://picsum.photos/seed/registry-amazon/400/200",
 		href: "#",
+		linkLabel: "Auf zur Wunschliste"
+	},
+	{
+		name: "Auf ein Wohl 💸",
+		imageUrl: "https://picsum.photos/seed/registry-paypal/400/200",
+		href: "#",
+		linkLabel: "paypal.me"
 	},
 ];
