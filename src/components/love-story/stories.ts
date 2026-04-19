87ad88f4acbfe0278ee29da29666c86b12e5c1a3
dennishenle <henledennis@gmail.com>
2026-04-19 20:45:05 +0200

    feat: fixed some typos

--- src/components/love-story/stories.ts
+++ src/components/love-story/stories.ts
@@ -20,7 +20,7 @@ export const stories = [
         title: "Der Antrag",
         text: `Nachdem das Wetter endlich gut genug war, um wieder auf das Rubihorn zu wandern, machten wir uns sofort auf den Weg. 
             Der Weg war wie immer sehr schön und wir machten mit einer süßen Kröte Bekanntschaft. Als wir nach dem steilen Ansteig 
-            endlich am Gipfelkreuz angelangt waen, wunderte ich mich sehr, dass Dennis wollte, 
+            endlich am Gipfelkreuz angelangt waren, wunderte ich mich sehr, dass Dennis wollte, 
             dass jemand ein Foto von uns macht (normalerweise mag er das gar nicht). Zu allem Überfluss wollte er dann auch noch, dass wir unsere Rucksäcke abnehmen, 
             obwohl ich einfach nur fertig war, mich setzen und etwas essen wollte. Er nahm trotzdem den Rucksack ab und holte ein kleines weißes Schächtelchen heraus. 
             Da wusste ich sofort, was er vorhat. Noch bevor er auf die Knie gehen konnte, hatte ich bereits Tränen in den Augen.
